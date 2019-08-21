dc4327b21cc4eaeece83991b716a080947f39125
Marcus Cliquet <cliquet@gmail.com>
2019-08-21 09:04:30 -0300

    insercao de usuario com POST

--- src/main/java/com/course/workshopmongo/services/UserService.java
+++ src/main/java/com/course/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.course.workshopmongo.domain.User;
+import com.course.workshopmongo.dto.UserDTO;
 import com.course.workshopmongo.repository.UserRepository;
 import com.course.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,11 @@ public class UserService {
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
 	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
 }
